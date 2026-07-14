6fed54b41533930fe3f22076658cfe4265616daa
Rishab <risha@example.com>
2026-07-14 18:18:02 +0530

    fix: open import choices dialog in handleUploadFileChange for Option 2 table upload

--- src/pages/MockSimulator.tsx
+++ src/pages/MockSimulator.tsx
@@ -192,6 +192,7 @@ const MockSimulator = () => {
   const handleUploadFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (files && files.length > 0) {
+      setIsUploadDialogOpen(true);
       await processUploadFile(files[0]);
     }
   };
